cf69f0c6d5329eaad2b36e174fcbcb107c166457
Zarina <83446775+ZarinaPi@users.noreply.github.com>
2024-05-18 17:27:37 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, "RUS");
+	int num;
+	cout << "Введите номер лабораторно работы которую хотите запустить: \n";
+	cin >> num;
+
+	switch (num) {
+	case 1:
+	{
+		system("LABA1.exe");
+		break;
+	}
+	case 2:
+	{
+		system("LABA2.exe");
+		break;
+	}
+	case 3:
+	{
+		system("LABA3.exe");
+		break;
+	}
+	default:
+		break;
+	}
+
+	return 0;
+}
